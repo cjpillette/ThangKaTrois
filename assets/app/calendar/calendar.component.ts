5028a68697ca3b7fb303ec4c924ca504368c98b1
Carole <cjpillette@hotmail.com>
2018-02-04 16:05:13 +1100

    Populating table with proper moments

--- assets/app/calendar/calendar.component.ts
+++ assets/app/calendar/calendar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit, ElementRef, Renderer2, ViewChild } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef, Renderer2, ViewChild, AfterViewChecked } from '@angular/core';
 import * as moment from 'moment';
 
 @Component({
@@ -7,56 +7,63 @@ import * as moment from 'moment';
   styleUrls: ['./calendar.component.css']
 })
 export class CalendarComponent implements OnInit {
+  public counter: number;
+  currentDate: moment.Moment; // for math
+  monthTitle: string; // for show
+  monthNumber: number; // for calculation
+
   @ViewChild('tableBody', {read: ElementRef}) tableBody: ElementRef;
 
-  // for the header
-  currentMonth: string = moment().locale("fr").format('MMMM');
-
-  // for subheader
   days: Array<string> = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
-  months: Array<string> = ['janvier', 'fevrier', 'mars', 'avril', 'mai', 'juin', 'juillet', 'aout', 'septembre', 'octobre', 'novembre', 'decembre'];
-  
+
   constructor(private renderer: Renderer2) { }
-  
-  ngOnInit() {}
-  
+
+  ngOnInit() {
+    this.counter = moment().month();
+    // initializing the month we're at as a moment for math and as a string for show (in the title)
+    this.currentDate = moment();
+    this.monthNumber = moment().month();
+    this.monthTitle = moment().locale('fr').format('MMMM'); // i.e fevrier
+  }
+
   ngAfterViewInit() {
-    const monthArray: Array<any> = this.getMonth('1')
+    const monthArray: Array<{week: number, days: Array<number>}> = this.getMonth(this.currentDate);
     this.displayHtmlMonthTable(monthArray);
-}
+  }
 
-  // getMonth argument is a string with number starting
-  // at 1 ending witih 12, ie. january shd be '1', february '2', dec '12'
-  getMonth(month: string){
-    let currentMonth = moment(month, "MM")
-    let monthlyCalendar = []
-    const startWeek = currentMonth.startOf('month').week();
-    const endWeek = currentMonth.endOf('month').week();
+  // ngAfterViewChecked() {
+  //   console.log('inside ngAfterViewChecked', this.currentDate);
+  // }
 
-    for(var week = startWeek; week<=endWeek;week++){
+  getMonth(mmt: moment.Moment) {
+    const monthlyCalendar = [];
+    const startWeek = mmt.startOf('month').week(); // week number in the year
+    const endWeek = mmt.endOf('month').week(); // week number in the year
+
+    for (let week = startWeek; week <= endWeek; week++) {
       monthlyCalendar.push({
-        week:week,
-        days:Array(7).fill(0).map((n, i) => moment().week(week).startOf('isoWeek').clone().add(n + i, 'day'))
-      })
+        week: week,
+        days: Array(7).fill(0).map((n, i) => moment().week(week).startOf('week').clone().add(n + i, 'day'))
+      });
     }
     return monthlyCalendar;
   }
 
-  displayHtmlMonthTable(monthArray: Array<{week: number, days:Array<number>}>){
+  displayHtmlMonthTable(monthArray: Array<{week: number, days: Array<number>}>) {
     // creating all cells
     for (let i = 0; i < monthArray.length; i++) {
       // creates a table row
-      let tr = this.renderer.createElement("tr");
+      const tr = this.renderer.createElement('tr');
 
       // 7 days of the week
       for (let j = 0; j < 7; j++) {
         // Create a <td> element and a text , make the text
         // node the contents of the <td>, and put the <td> at
         // the end of the table row
-        let td = this.renderer.createElement("td");
-        let momentMoment: number = monthArray[i]['days'][j];
-        let momentString: string = momentMoment.toString();
-        let cellText = this.renderer.createText(momentString);
+        const td = this.renderer.createElement('td');
+        const momentMoment: number = monthArray[i]['days'][j];
+        const momentString: string = momentMoment.toString();
+        const cellText = this.renderer.createText(momentString);
         this.renderer.appendChild(td, cellText);
         this.renderer.appendChild(tr, td);
       }
@@ -66,16 +73,15 @@ export class CalendarComponent implements OnInit {
     }
   }
 
-  getCurrentNumMonth() {
-    return this.months.indexOf(this.currentMonth) + 1;
-  }
+    increment() {
+      const nextMonth = this.currentDate.add(1, 'M');
+      this.getMonth(nextMonth);
+      this.currentDate = nextMonth;
+    }
 
-  changeDate = (difference) => {
-    moment().add(difference, 'months')
-  }
-
-  changeMonth = (difference) => {
-    this.changeDate(difference);
-  }
+    decrement() {
+      const lastMonth = this.currentDate.subtract(1, 'M');
+      console.log('last month', lastMonth);
+    }
 
 }
