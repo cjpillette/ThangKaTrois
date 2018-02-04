8d894a1cbf5856eb89b6d6eaf5c2f8f0305a04e8
Carole <cjpillette@hotmail.com>
2018-02-04 14:27:16 +1100

    TSlint: fixed lint errors

--- assets/app/app.routing.ts
+++ assets/app/app.routing.ts
@@ -1,14 +1,14 @@
-import { Routes, RouterModule } from "@angular/router";
+import { Routes, RouterModule } from '@angular/router';
 
-import { HomepageComponent } from "./homepage/homepage.component";
-import { AuthenticationComponent } from "./auth/authentication.component";
-import { SitesComponent } from "./area/sites/sites.component";
-import { TravelsComponent } from "./area/travels/travels.component";
-import { WeatherComponent } from "./area/weather/weather.component";
-import { HousingComponent } from "./area/housing/housing.component";
-import { CalendarComponent } from "./calendar/calendar.component";
-import { TrainingsComponent } from "./trainings/trainings.component";
-import { AUTH_ROUTES } from "./auth/auth.routes";
+import { HomepageComponent } from './homepage/homepage.component';
+import { AuthenticationComponent } from './auth/authentication.component';
+import { SitesComponent } from './area/sites/sites.component';
+import { TravelsComponent } from './area/travels/travels.component';
+import { WeatherComponent } from './area/weather/weather.component';
+import { HousingComponent } from './area/housing/housing.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { TrainingsComponent } from './trainings/trainings.component';
+import { AUTH_ROUTES } from './auth/auth.routes';
 
 const APP_ROUTES: Routes = [
     { path: '', component: HomepageComponent },
