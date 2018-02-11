71a175ff6cf3bd104b427d30c17e5a2e55aa72e2
Carole <cjpillette@hotmail.com>
2018-02-11 15:01:12 +1100

    Added keywords, title and description to improve SEO

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
-import { TrainingService } from "./trainings/training.service";
+import { TrainingService } from './trainings/training.service';
 
 
 @Component({
@@ -10,4 +11,15 @@ import { TrainingService } from "./trainings/training.service";
   providers: [TrainingService]
 })
 export class AppComponent {
+  constructor(meta: Meta, title: Title) {
+
+    title.setTitle('Parapente Thank=g-Ka, baptême, vol biplace, stage en Cantal.');
+
+    meta.addTags([
+      { name: 'author',   content: 'Thang-ka'},
+      { name: 'keywords', content: 'Ecole, bon cadeau, parapente, stage, Cantal, Auvergne, Puy Mary, Plomb du Cantal, Lioran, Aurillac, vol biplace, bapteme de l\'air, paragliging'},
+      { name: 'description', content: 'Ecole de parapente dans le Cantal Auvergne France. Parapente et vol libre. Baptême de l\'air en parapente et Ulm pendulaire. Vols biplaces. Stage initiation, formation, progression au parapente, paramoteur, et cage de pilotage, Ulm pendulaire. Boutique parapente. Matériel neuf ou occasion. Ecole de parapente Thang-Ka en Auvergne-Puy Mary-Plomb du Cantal-Puy de Dôm. Cantal et Massif Central. Bon cadeau. Voyages. Paragliding school. Parapente school in de Cantal Auvergne Frankrijk.' }
+    ]);
+
+  }
 }
